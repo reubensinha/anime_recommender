23dd6f55b72edabb3b375b808d67e13406a1cdf1
Reuben Sinha <reuben.sinha@gmail.com>
2023-07-17 19:22:05 -0700

    Unzips dataset and deletes zipfile

--- kaggle_dataset.py
+++ kaggle_dataset.py
@@ -4,11 +4,20 @@
 # https://www.kaggle.com/datasets/dbdmobile/myanimelist-dataset
 
 # Authenticate kaggle API
+import os
+import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 api = KaggleApi()
 api.authenticate()
 
-## Dowload Datasets
-
+## Dowload Dataset
 # List and information about the different attributes and characteristics of each anime.
 api.dataset_download_file("dbdmobile/myanimelist-dataset", file_name='anime-filtered.csv', path='./data/')
+
+# Unzip file
+with zipfile.ZipFile('./data/anime-filtered.csv.zip', 'r') as zipref:
+    zipref.extractall('./data/')
+
+# Delete zip file
+if os.path.exists('./data/anime-filtered.csv.zip'):
+    os.remove('./data/anime-filtered.csv.zip')
