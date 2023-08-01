b4aad07c905db7e87eeab1f687ede5f4cca81700
Reuben Sinha <reuben.sinha@gmail.com>
2023-08-01 16:12:52 -0700

    Completed Menu Selection

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 import MAL_user_data
 import kaggle_dataset
 
-
-def select_input():
-    # TODO:
+def rec_anime():
+    #TODO:
     return
+
+
+def select_input(select):
+    match select:
+        case 0:
+            print("Shuting down...")
+        case 1:
+            rec_anime()
+        # case 2: TODO
+        #     return 
+        case _:
+            print("Invalid Selection\n")
 
 
 def list_menu():
-    # TODO:
-    return
+    print("\n1: Recommend Anime" + 
+        # "\n2: Recommend Manga" + TODO
+        "\n0: Exit")
+    
+    return int(input("\nEnter Selection: "))
 
 
 def main():
@@ -21,8 +35,7 @@ def main():
 
     int(select)
     while select != 0:
-        list_menu()
-        select = int(input("Enter Selection: "))
+        select = list_menu()
         select_input(select)
 
 
