12deddd3d5aac62d5165d4a5e18292c7ec90efdb
Reuben Sinha <reuben.sinha@gmail.com>
2023-08-16 14:32:29 -0700

    Replaced anime-filtered.csv with anime-dataset-2023.csv for more updated infromation.
    Also added download for users-score-2023.csv

--- kaggle_dataset.py
+++ kaggle_dataset.py
@@ -14,13 +14,21 @@ def load_set():
 
     ## Dowload Dataset
     # List and information about the different attributes and characteristics of each anime.
-    api.dataset_download_file("dbdmobile/myanimelist-dataset", file_name='anime-filtered.csv', path='./data/')
+    api.dataset_download_file("dbdmobile/myanimelist-dataset", file_name='anime-dataset-2023.csv', path='./data/')
+    # WARNING LARGE FILE
+    api.dataset_download_file("dbdmobile/myanimelist-dataset", file_name='users-score-2023.csv', path='./data/')
+    
+
 
     # Unzip file
-    with zipfile.ZipFile('./data/anime-filtered.csv.zip', 'r') as zipref:
+    with zipfile.ZipFile('./data/anime-dataset-2023.csv', 'r') as zipref:
+        zipref.extractall('./data/')
+    with zipfile.ZipFile('./data/users-score-2023.csv', 'r') as zipref:
         zipref.extractall('./data/')
 
     # Delete zip file
-    if os.path.exists('./data/anime-filtered.csv.zip'):
-        os.remove('./data/anime-filtered.csv.zip')
+    if os.path.exists('./data/anime-dataset-2023.csv.zip'):
+        os.remove('./data/anime-dataset-2023.csv.zip')
+    if os.path.exists('./data/users-score-2023.csv.zip'):
+        os.remove('./data/users-score-2023.csv.zip')
 
